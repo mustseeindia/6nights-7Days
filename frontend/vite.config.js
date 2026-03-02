fff9fab007d2012bba6d8582a9b88e22ff8c474a
Karthik <karthik@indiatourpackages.com>
2026-03-02 08:00:24 +0530

    Update base URL in Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ process.env.STORAGE_GATEWAY_URL =
   process.env.STORAGE_GATEWAY_URL || "https://blob.caffeine.ai";
 
 export default defineConfig({
-  base: '/kerala/4n-5d/',
+  base: '/kerala/6n-7d/',
   logLevel: "error",
   build: {
     emptyOutDir: true,
